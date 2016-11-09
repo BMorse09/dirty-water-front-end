e619ab13e4911aa882ed209a74dd23c59bd51903
BMorse09 <benmorse09@gmail.com>
2016-11-09 18:45:00 -0500

    Still working on delete button for reviews Completed README

--- app/components/dw-location/component.js
+++ app/components/dw-location/component.js
@@ -4,5 +4,9 @@ export default Ember.Component.extend({
   auth: Ember.inject.service(),
   isCurrentUser: Ember.computed.alias('auth.credentials.id'),
 
-
-});
+  actions: {
+    deleteItem () {
+    return this.sendAction('deleteItem');
+      },
+    },
+  });
